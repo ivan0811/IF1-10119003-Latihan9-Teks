d1355c2b348d7fc69529ec7baf36073970325625
ivan0811 <ivanfaathirza@gmail.com>
2020-10-16 18:19:29 +0700

    initial commit

---
+++ src/com/teks/Teks.java
@@ -0,0 +1,18 @@
+package com.teks;
+
+/*
+	NAMA = Ivan Faathirza
+	KELAS = IF1
+	NIM = 10119003
+	Deskripsi program = program ini untuk menampilkan teks dengan tab \t dan baris baru \n
+ */
+public class Teks {
+
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char:"+tab+"berisi "+c);
+    }
+}
